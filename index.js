1483be954ee25a6763a46b8a07f84d58a4c98010
saisankar <101086279+saisankar-1919@users.noreply.github.com>
2023-07-10 22:28:13 +0530

    google Oauth configuration

--- index.js
+++ index.js
@@ -4,10 +4,47 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 7996;
 const authenticationRoute = require('./src/routes/auth')
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+require('./src/helpers/passport');
 
 app.use(express.json()); // Example middleware for JSON parsing
 
 app.use('/api', authenticationRoute)
+
+app.get("/", (req, res) => {
+    res.json({message: "You are not logged in"})
+})
+
+app.get("/failed", (req, res) => {
+    res.send("Failed")
+})
+app.get("/success", (req, res) => {
+    res.send(`Welcome ${req.user.email}`)
+})
+
+app.get('/google',
+    passport.authenticate('google', {
+            scope:
+                ['email', 'profile']
+        }
+    ));
+
+app.get('/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/failed',
+    }),
+    function (req, res) {
+        res.redirect('/success')
+
+    }
+);
+
+app.get("/logout", (req, res) => {
+    req.session = null;
+    req.logout();
+    res.redirect('/');
+})
 
 // Error handling middleware
 app.use((err, req, res, next) => {
